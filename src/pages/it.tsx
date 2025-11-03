dd39c3ce562fb36f401c9da5641e8c30c2ced8d7
Dan-The-Assassin <dantheassassin1702@gmail.com>
2025-11-04 01:59:20 +0200

    Progress game dev

--- src/pages/it.tsx
+++ src/pages/it.tsx
@@ -20,9 +20,11 @@ function IT() {
       <PcIcon anchorPath='#Databases' image={DatabaseIcon} text="Databases"></PcIcon>
       </div>
       <h1 id="GameDev">Game Development</h1>
-      <h2>FarmSouls</h2>
+      <h2 className="gameTitle">FarmSouls (Thesis Project)</h2>
       <p>[ 05/2025 – 26/06/2025 ]</p>
-      <p>
+      <iframe className="FSVideo" width="560" height="315" src="https://www.youtube.com/embed/4XTroj3vDvk?si=nmOATANfyq1MO0WV" title="YouTube video player" allow="accelerometer; autoplay; 
+clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+      <p className="gameDescription">
         A 2D game that blends together elements from the farming simulator genre with combat
 mechanics, resulting in a unique combination. The player is tasked with taking care of a
 farm, where the plants turn into enemies at the time of harvest. The game presents
@@ -36,14 +38,48 @@ similar state machine, a farming system, a custom easily modifiable and extendab
 resource for plants, custom functions for time, scene and combat-based music, and a
 save system that retains all inventory, farming and player related info.
       </p>
-      <iframe width="420" height="315"
-src="https://youtu.be/4XTroj3vDvk">
-</iframe>
       <p>Developed for PC in Godot using C#.</p>
       <h2>VR Boat Game</h2>
-      <h2>TrueBlood</h2>
+      <p>[ 18/01/2025 – 15/04/2025 ]</p>
+      <p className="gameDescription">A VR game that simulates boat rowing, made for Politehnica University of Bucharest's
+TRAVEE (Virtual Therapist with Augmented Feedback for Neuromotor Recovery) Project,
+which aims to help the recovery process of patients with neuromotor impairments
+following strokes, traumas or brain surgery. The player can manipulate the boat's oars
+using a customizable rowing motion, being tasked with getting to a finish line and
+colliding with collectibles along the way. The game offers plenty of settings: the boat's
+speed, the width of the rowing motion, the player position, the oars' sensitivity to hand
+movement etc.</p>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/ry7KRNBON04?si=OhhW1TEjresR_9SK" title="YouTube video player" allow="accelerometer; autoplay; 
+clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+      <p>Developed for Meta Quest 2&3 in Unity using C# as well as the XR Unity assets.</p>
+      <h2>TrueBlood (Thesis Project)</h2>
+      <p>[ 02/2023 – 06/2023 ]</p>
+      <p className="gameDescription">A simple 2D "metroidvania" game with 20+ scenes, giving the player the goal of exploring
+the map, fighting enemies and going through platforming sections in order to reach the
+final scene, unlocking abilities and upgrades along the way. It also features systems such
+as: player state machine, saving and loading, keyboard and controller support, menus
+and UI, audio and video settings, combat, simple enemy AI, parallax scrolling, custom
+collisions, movement such as variable height jump, dashing and teleporting.</p>
+      <p>Developed for PC in Unity using C#</p>
       <h2>Beat Dungeon</h2>
+      <p>[ 21/04/2019 – 08/06/2019 ]</p>
+      <p className="gameDescription">A 2D rhythm-based game made for Ubisoft's "Gamecelerator 6.0" contest, where the
+player has to go through 2 levels each with their own visual theme, finding the exit in
+each. Having the position of team leader and lead programmer, I've made the following
+systems: a player health system, a rhythm system with UI that shows the incoming beats,
+2 scenes with various rhythm-based obstacles such as spikes, spinning blades, arrows and
+lasers.
+</p>
+      <p>Developed for PC in Unity using C#</p>
       <h2>Airplane Racer</h2>
+      <p>[ 19/01/2024 – 19/01/2024 ]</p>
+      <p className="gameDescription">A simple 3D airplane game made in 24 hours, where the player has to complete 3 laps
+through a racing area, avoiding trees and other obstacles, collecting coins for score and
+trying to reach the end as fast as possible. The game also features the following systems:
+a main menu with trophies for every win and an option to change the plane, a save
+system, checkpoints, and a booster system allowing the player to speed up for a short
+while.</p>
+      <p>Developed for PC in Unity using C#.</p>
       <h1 id="Graphics">Graphics</h1>
       <h2>3D Graphics Engine (OpenGL)</h2>
       <h1 id="WebDev">Web Development</h1>
