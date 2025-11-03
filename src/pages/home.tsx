b8b430f10093e7494215f763263ce71664011e2f
Dan-The-Assassin <dantheassassin1702@gmail.com>
2025-11-04 00:22:23 +0200

    Skills added

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,6 @@
 //import { useState } from 'react'
 import './home.css'
+import FSMC from '../assets/FarmSoulsMC.gif'
 
 
 function Home() {
@@ -12,13 +13,50 @@ function Home() {
       <p>
         Hello! I'm Daniel, a Romanian game developer and artist! This is my portfolio, where you can find information about most of the projects I've worked on, be it programming, 
         music or art.</p>
-      <p className= "FSMC"> 
+      <p className="FSMC"> 
         I'm a master's degree graduate in Graphics, Multimedia and Virtual Reality, having extensive programming experience. I've worked on various projects, such as "Beat Dungeon",
-        a rhythm-based rogue-like that reached top 10 in Ubisoft's "Gamecelerator 6.0" competition and database work at BirdLife Romania (SOR).</p>
+        a rhythm-based rogue-like that reached top 10 in Ubisoft's "Gamecelerator 6.0" competition, and database work at BirdLife Romania (SOR).</p>
       <p className= "TBMC">
-        Below you'll find a list of skills, tools, frameworks and engines that I'm knowledgeable about, as well as ways to contact me. If you're interested in working
-        with me for a project, please check the respective page of what you're interested in first for more information!
+        Below you'll find a list of programming languages, tools, frameworks and engines that I'm knowledgeable about, as well as how to contact me. Interested in working
+        with me? Please check the page of what you're interested in first for more information!
       </p>
+      <table className="skillsGrid">
+      <tr>
+        <th>Languages</th>
+        <th>Engines/Frameworks</th>
+        <th>Tools</th>
+      </tr>
+      <tr>
+        <td>C#</td>
+        <td>Godot</td>
+        <td>FL Studio</td>
+      </tr>
+      <tr>
+        <td>C++</td>
+        <td>Unity</td>
+        <td>Clip Studio Paint</td>
+      </tr>
+      <tr>
+        <td>JavaScript/TypeScript</td>
+        <td>GameMaker Studio 2</td>
+        <td>Photoshop/AfterEffects</td>
+      </tr>
+      <tr>
+        <td>HTML</td>
+        <td>OpenGL</td>
+        <td>Blender</td>
+      </tr>
+      <tr>
+        <td>CSS/SCSS</td>
+        <td>React</td>
+        <td>GitHub</td>
+      </tr>
+      <tr>
+        <td></td>
+        <td>Angular</td>
+        <td>FMOD</td>
+      </tr>
+      </table>
     </div>
   )
 }
