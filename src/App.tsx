e41f8cc4929c966189facac8975e4b0e3a09bc35
Dan-The-Assassin <dantheassassin1702@gmail.com>
2025-11-09 17:56:28 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,6 @@ function App() {
 
   return (
     <>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     {/*<BrowserRouter>
       <Routes>
         <Route path ="/IT" element ={<IT/>}/>
