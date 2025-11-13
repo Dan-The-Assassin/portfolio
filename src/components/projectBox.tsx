4ab47a565520103ff793d5f319d4658f799daa50
Dan-The-Assassin <dantheassassin1702@gmail.com>
2025-11-13 19:23:01 +0200

    Mobile trying to fix IT page

--- src/components/projectBox.tsx
+++ src/components/projectBox.tsx
@@ -30,7 +30,7 @@ function projectBox({titleText, dateText, devInfo, shortDescription, depthDescri
     function renderMedia(){
         if(videoLink!="")
         {
-            return(<iframe width="560" height="315" src={videoLink} title="YouTube video player" allow="accelerometer; autoplay; 
+            return(<iframe src={videoLink} title="YouTube video player" allow="accelerometer; autoplay; 
             clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>)
         }
         if(imagePath!="")
