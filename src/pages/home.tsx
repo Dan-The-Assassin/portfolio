04eda40d4f18121be17cb77e31799b5eb3dc57ce
Dan-The-Assassin <dantheassassin1702@gmail.com>
2025-11-18 20:50:34 +0200

    Update home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -33,8 +33,9 @@ function Home() {
         Hello! I'm Daniel, a Romanian game developer and artist! This is my portfolio, where you can find information about most of the projects I've worked on, be it programming, 
         music or art.</p>
       <p className="FSMC"> 
-        I'm a master's degree graduate in Graphics, Multimedia and Virtual Reality, having extensive programming experience. I've worked on various projects, such as "Beat Dungeon",
-        a rhythm-based rogue-like that reached top 10 in Ubisoft's "Gamecelerator 6.0" competition, and database work at BirdLife Romania (SOR).</p>
+        I have graduated from the Politehnica University of Bucharest with a master's degree in Graphics, Multimedia and Virtual Reality and have extensive programming experience, 
+        focusing mainly on Object-Oriented Programming. I've also worked on various projects, such as "Beat Dungeon", a rhythm-based rogue-like that reached top 10 in Ubisoft's 
+        "Gamecelerator 6.0" competition, and database work at BirdLife Romania (SOR).</p>
       <p className= "TBMC">
         Below you'll find a list of programming languages, tools, frameworks and engines that I'm knowledgeable about, as well as how to contact me. Interested in working
         with me? Please check the page of what you're interested in first for more information!
