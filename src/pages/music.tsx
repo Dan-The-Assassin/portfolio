dad93d243f8be9673fb56fcb93a95a48fec0c12a
Dan-The-Assassin <dantheassassin1702@gmail.com>
2026-01-31 15:38:07 +0200

    Music page update 2

--- src/pages/music.tsx
+++ src/pages/music.tsx
@@ -11,8 +11,9 @@ function Music() {
       <h2>Sections</h2>
       <ProjectBox titleText="Music compilation" dateText="2020 - 2025" devInfo= "Made using FL Studio."
       shortDescription="A video showcasing the various genres of music I have the knowledge and skills to compose and produce."
-      depthDescription="" 
-      depthDescription2=""
+      depthDescription="All of the music presented in the video has been produced digitally, only using FL Studio and many of the plugins available for it. I am available for music
+      commissions, simply contact me and mention what you need. If you don't see the genre you're interested in here, don't worry, just ask, there's several more I can do or try!" 
+      depthDescription2="If trying to commission me, PLEASE provide clear descriptions of what you need AND references if possible, this will make it a lot more likely I'll accept!"
       videoLink="https://www.youtube.com/embed/mrEX-PCIafA?si=Q4OaLSt6DspNrerO" imagePath=""></ProjectBox>
     </div>
 </>
