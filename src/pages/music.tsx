3d2b507996b6976614b51beb55a7c5ce03e01c66
Dan-The-Assassin <dantheassassin1702@gmail.com>
2026-01-31 15:20:29 +0200

    Update music.tsx

--- src/pages/music.tsx
+++ src/pages/music.tsx
@@ -1,3 +1,4 @@
+import './music.css'
 import ProjectBox from '../components/projectBox'
 
 function Music() {
@@ -8,20 +9,13 @@ function Music() {
     <div className="musicMain">
       <h1>Music</h1>
       <h2>Sections</h2>
-              {/*<div className="icons">
-
-      <PcIcon anchorPath='#GameDev' image={GameIcon} text="GameDev"></PcIcon>
-      <PcIcon anchorPath='#Graphics' image={GraphicsIcon} text="Graphics"></PcIcon>
-      <PcIcon anchorPath='#WebDev' image={WebDevIcon} text="WebDev"></PcIcon>
-      <PcIcon anchorPath='#Databases' image={DatabaseIcon} text="Databases"></PcIcon>
-      </div>*/}
       <ProjectBox titleText="Music compilation" dateText="2020 - 2025" devInfo= "Made using FL Studio."
       shortDescription="A video showcasing the various genres of music I have the knowledge and skills to compose and produce."
       depthDescription="" 
       depthDescription2=""
       videoLink="https://www.youtube.com/embed/mrEX-PCIafA?si=Q4OaLSt6DspNrerO" imagePath=""></ProjectBox>
-      </div>
-    </>
+    </div>
+</>
   )
 }
 
